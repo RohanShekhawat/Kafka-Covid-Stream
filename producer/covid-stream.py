04d78086caa4750e7446b93ddfbaa34f860c3d9d
Tanmay Mittal <32292953+strikertanmay@users.noreply.github.com>
2020-05-26 02:50:25 +0530

    Update covid-stream.py

--- producer/covid-stream.py
+++ producer/covid-stream.py
@@ -18,8 +18,8 @@ def producer_trigger(raw_data, context):
     state_stats_url = ('https://api.covid19india.org/data.json')
     district_stats_url = ('https://api.covid19india.org/v2/state_district_wise.json')
     bootstrap_servers = "localhost:9092"
-    kafka_district_data_topic_name = "distict-data"
-    kafka_state_data_topic_name = "state-data"
+    kafka_district_data_topic_name = "district-data"
+    kafka_processed_data_topic_name = "processed-data"
    
 
     conf = {'bootstrap.servers': bootstrap_servers}
@@ -41,7 +41,7 @@ def producer_trigger(raw_data, context):
                           'notes': dd['notes']
                           })
             try:
-                producer.produce(topic=kafka_district_state_data_topic_name, value=json.dumps(value), key=json.dumps(key),
+                producer.produce(topic=kafka_district_data_topic_name, value=json.dumps(value), key=json.dumps(key),
                                  on_delivery=fail)
             except BufferError:
                 logger.error('%% Local producer queue is full (%d messages awaiting delivery): try again\n' %
@@ -50,21 +50,39 @@ def producer_trigger(raw_data, context):
     logger.info('%% Waiting for %d deliveries\n' % len(producer))
     producer.flush()
 
-    state_data = requests.get(state_stats_url).json()
-    for k in state_data['statewise']:
-        state = k['state']
-        key = dict({'state': state})
-        value = dict({'state': state, 'active': k['active'], 'confirmed': k['confirmed'],
-                'recovered': k['recovered'], 'deceased': k['deaths'],
-                'last_updated' : k['lastupdatedtime']
-                })
-        try:
-            producer.produce(topic=kafka_current_data_topic_name, value=json.dumps(value), key=json.dumps(key),
-                            on_delivery=fail)
-        except BufferError:
-            logger.error('%% Local producer queue is full (%d messages awaiting delivery): try again\n' %
+    district_data = requests.get(district_stats_url).json()
+    for data in district_data:
+        state = data['state']
+        district_data = data['districtData']
+        finalDict = {}
+        for dd in district_data:
+            district = dd['district']
+            key = dict({'state': state, 'district': district})
+            if(dd['active'] < 200):
+                finalDict.update({ 'low_risk_zone' : dict({'state': state, 'district': district, 'active': dd['active'],'confirmed': dd['confirmed'],
+                          'recovered': dd['recovered'], 'deceased': dd['deceased'],
+                          'deltaConfirmed': dd['delta']['confirmed'],
+                          'deltaRecovered': dd['delta']['recovered'], 'deltaDeceased': dd['delta']['deceased'],
+                          'notes': dd['notes']})})
+            elif(dd['active'] > 200 and dd['active'] < 800):
+                finalDict.update({ 'moderate_risk_zone' : dict({'state': state, 'district': district, 'active': dd['active'] , 'confirmed': dd['confirmed'],
+                          'recovered': dd['recovered'], 'deceased': dd['deceased'],
+                          'deltaConfirmed': dd['delta']['confirmed'],
+                          'deltaRecovered': dd['delta']['recovered'], 'deltaDeceased': dd['delta']['deceased'],
+                          'notes': dd['notes']})})
+            elif(dd['active'] > 800):
+                finalDict.update({ 'high_risk_zone' : dict({'state': state, 'district': district, 'active': dd['active'] , 'confirmed': dd['confirmed'],
+                          'recovered': dd['recovered'], 'deceased': dd['deceased'],
+                          'deltaConfirmed': dd['delta']['confirmed'],
+                          'deltaRecovered': dd['delta']['recovered'], 'deltaDeceased': dd['delta']['deceased'],
+                          'notes': dd['notes']})})
+            try:
+                producer.produce(topic=kafka_processed_data_topic_name, value=json.dumps(finalDict), key=json.dumps(key),
+                                 on_delivery=fail)
+            except BufferError:
+                logger.error('%% Local producer queue is full (%d messages awaiting delivery): try again\n' %
                              len(producer))
-        producer.poll(0)
+            producer.poll(0)
     logger.info('%% Waiting for %d deliveries\n' % len(producer))
     producer.flush()
 
